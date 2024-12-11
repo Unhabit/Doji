db567b5d8e37a63d3a30502bfe078a9e92b21d78
Nahid Shovo <shovonahid37@gmail.com>
2024-12-11 10:51:19 -0500

    search bar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 from dynaconf import Dynaconf
 
@@ -26,13 +26,23 @@ def index():
 
 
 @app.route("/browse")
-def produc_browse():
+def product_browse():
+
+    query = request.args.get('query')
+
     conn = connect_db()
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM `Product` ;")
+
+    if query is None:
+        cursor.execute("SELECT * FROM  `Product` ;")
+    else:
+        cursor.execute(f"SELECT * FROM `Product` WHERE `name` LIKE '%{query}%' ;")
 
     results = cursor.fetchall()
 
-    return render_template("browse.html.jinja", products = results)
+    cursor.close()
+    conn.close()
+
+    return render_template("browse.html.jinja", products = results, query=query)
 
